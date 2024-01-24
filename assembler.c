2f05282b4d2fb41c011226012939c9ce9c4758fd
drunkmonk21 <dhruvbansal04@gmail.com>
2024-01-24 11:59:49 -0600

    Inital Commit
    These are the starter files from the lab doc

---
+++ assembler.c
@@ -0,0 +1,188 @@
+/*
+	Name 1: Dhruv Bansal
+	Name 2: 
+	UTEID 1: db45988
+	UTEID 2: 
+*/
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+
+#define MAX_LABEL_LEN 20
+#define MAX_SYMBOLS 255
+typedef struct {
+	int address;
+	char label[MAX_LABEL_LEN + 1];	/* Question for the reader: Why do we need to add 1? */ // because we need to append a null character to the end of each symbol
+} TableEntry;
+TableEntry symbolTable[MAX_SYMBOLS];
+
+
+int toNum( char * pStr )
+{
+   char * t_ptr;
+   char * orig_pStr;
+   int t_length,k;
+   int lNum, lNeg = 0;
+   long int lNumLong;
+
+   orig_pStr = pStr;
+   if( *pStr == '#' )				/* decimal */
+   { 
+     pStr++;
+     if( *pStr == '-' )				/* dec is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isdigit(*t_ptr))
+       {
+	 printf("Error: invalid decimal operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNum = atoi(pStr);
+     if (lNeg)
+       lNum = -lNum;
+ 
+     return lNum;
+   }
+   else if( *pStr == 'x' )	/* hex     */
+   {
+     pStr++;
+     if( *pStr == '-' )				/* hex is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isxdigit(*t_ptr))
+       {
+	 printf("Error: invalid hex operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNumLong = strtol(pStr, NULL, 16);    /* convert hex string into integer */
+     lNum = (lNumLong > INT_MAX)? INT_MAX : lNumLong;
+     if( lNeg )
+       lNum = -lNum;
+     return lNum;
+   }
+   else
+   {
+	printf( "Error: invalid operand, %s\n", orig_pStr);
+	exit(4);  /* This has been changed from error code 3 to error code 4, see clarification 12 */
+   }
+}
+
+
+#define MAX_LINE_LENGTH 255
+	enum{
+	   DONE, OK, EMPTY_LINE
+	};
+
+	int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4){
+	   char * lRet, * lPtr;
+	   int i;
+	   if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) ) return( DONE );
+	   for( i = 0; i < strlen( pLine ); i++ ) pLine[i] = tolower( pLine[i] );
+	   
+           /* convert entire line to lowercase */
+	   *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+	   /* ignore the comments */
+	   lPtr = pLine;
+
+	   while( *lPtr != ';' && *lPtr != '\0' &&
+	   *lPtr != '\n' ) 
+		lPtr++;
+
+	   *lPtr = '\0';
+	   if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
+		return( EMPTY_LINE );
+
+	   if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+	   {
+		*pLabel = lPtr;
+		if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	   }
+	   
+           *pOpcode = lPtr;
+
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	   
+           *pArg1 = lPtr;
+	   
+           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg2 = lPtr;
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg3 = lPtr;
+
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg4 = lPtr;
+
+	   return( OK );
+	}
+
+	/* Note: MAX_LINE_LENGTH, OK, EMPTY_LINE, and DONE are defined values */
+
+    /*
+    func() 
+	{
+	   char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1,
+	        *lArg2, *lArg3, *lArg4;
+
+	   int lRet;
+
+	   FILE * lInfile;
+
+	   lInfile = fopen( "data.in", "r" );
+
+	   do
+	   {
+		lRet = readAndParse( lInfile, lLine, &lLabel,
+			&lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
+		if( lRet != DONE && lRet != EMPTY_LINE )
+		{
+			...
+		}
+	   } while( lRet != DONE );
+	}
+    */
+
+FILE* infile = NULL;
+FILE* outfile = NULL;
+
+int main(int argc, char* argv[]) {
+	
+     /* open the source file */
+     infile = fopen(argv[1], "r");
+     outfile = fopen(argv[2], "w");
+		 
+     if (!infile) {
+       printf("Error: Cannot open file %s\n", argv[1]);
+       exit(4);
+		 }
+     if (!outfile) {
+       printf("Error: Cannot open file %s\n", argv[2]);
+       exit(4);
+     }
+
+     /* Do stuff with files */
+
+     fclose(infile);
+     fclose(outfile);
+}
